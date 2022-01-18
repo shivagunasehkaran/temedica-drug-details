3fbdf2e68ed3bfc488b4774117e733651372aa69
Shiva Gunasehkaran <shivagautham8@gmail.com>
2022-01-18 20:38:31 +0530

    Add component on Home screen

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
-import {styles} from './Home.style';
+import React, {useState} from 'react';
+import {SafeAreaView, Keyboard} from 'react-native';
+import InputText from '../components/InputText';
 
 const Home = () => {
+  const [keyword, setKeyword] = useState('');
+  const [isClearButtonShown, setClearButtonShown] = useState(false);
+
+  const searchFilterFunction = (text) => {
+    setKeyword(text);
+  };
+
+  // on focus listener
+  const onInputFocusHandler = () => {
+    // flag for clear button
+    setClearButtonShown(true);
+  };
+
+  // clear text input
+  const handleTextInputClear = () => {
+    setKeyword('');
+  };
+
+  // calling api for on submit (keyboard)
+  const onSubmitHandler = () => {
+    Keyboard.dismiss();
+  };
+    
   return (
-    <View style={styles.container}>
       <SafeAreaView>
-        <View>
-          <Text>{'Hi'}</Text>
-        </View>
+        <InputText
+          value={keyword}
+          onChangeHandler={val => searchFilterFunction(val)}
+          onFocus={onInputFocusHandler}
+          isClearButtonShown={isClearButtonShown}
+          onClear={handleTextInputClear}
+          onSubmit={onSubmitHandler}
+        />
       </SafeAreaView>
-    </View>
   );
 };
 
